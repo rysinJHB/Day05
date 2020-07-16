745e9b9e45104f7257272e9880d62e373effc643
Ryan <sinryan7@gmail.com>
2020-07-16 20:33:26 +0200

    Third commit

--- ex02/ft_strstr.c
+++ ex02/ft_strstr.c
@@ -1,24 +1,30 @@
-#include <stdio.h>
 #include <unistd.h>
+#include <stdio.h>
 
-char *ft_strstr(char *str, char *to_find)
+
+
+char	*ft_strstr(char *str, char *to_find)
 {
-	int i;
-	int j;
 
-	i = 0;
-	while (str[i] != '\0')
-	{
-		j = 0;
-		while (to_find[j] == str[i + j])
-		{
-			if (to_find[j + 1] == '\0')
-			{
-				return (str + i);
-			}
-			j++;
-		}
-		i++;
-	}
-	return (0);
+  int i;
+  int j;
+  int to_find_length;
+  
+  to_find_length = -1;
+  while (to_find[++to_find_length]);
+
+  i = 0;
+  while (str[i])
+  {
+    j = 0;
+    while (str[i+j] == to_find[j])
+      j++;
+    if (j == to_find_length)
+      return (str + i);
+    i++;
+  }
+ 
+  
+  return(0);
 }
+
