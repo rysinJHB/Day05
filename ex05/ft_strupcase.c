116694272a4ba4e573576b126730e2c4c2e19089
Ryan <sinryan7@gmail.com>
2020-07-16 21:19:47 +0200

    Fifth commit

---
+++ ex05/ft_strupcase.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+char *ft_strupcase(char *str)
+{
+    int i;
+    
+    i = 0;
+    
+    if((str[i] >= 'a') && (str[i] <= 'z'))
+    {
+        str[i] -= 32;
+        i++;
+    }
+    
+    while (str[i])
+    {
+        if (str[i] == ' ')
+        {
+            i++;
+            str[i] -= 32;
+        }
+        
+        i++;
+    }
+    
+    return (str); 
